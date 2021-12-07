e639a11398aa67a1cd64e6e372894ad179bc3d40
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-12-06 20:37:50 -0500

    added GET handler AController reg form

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/LoginFormDTO.java
@@ -0,0 +1,33 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class LoginFormDTO {
+    @NotNull
+    @NotBlank
+    @Size(min = 7, max = 20, message ="size incorrect")
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 7, max = 20, message ="size incorrect")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
